e1e69f67d3579f9d28c5c2b08ecbb452a2a3771c
George Wong <wgeorge@fortinet.com>
2025-12-05 17:35:38 -0800

    checkin

---
+++ secret.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	jwt_token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+	aws_access_secret := "RJLgneKIiDn+rvQDkF8zZ3OqOy2+E2f0u5zuqB0s"
+	postgresql_uri := "postgresql://user:password@localhost:5432/mydb"
+}
